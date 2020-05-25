fb4954ac364024dd21bb7095b63dd52076b781f7
multidest <multidest@users.noreply.github.com>
2020-05-25 00:26:54 -0400

    Update recognizers.ts

--- core/src/utils/gesture/recognizers.ts
+++ core/src/utils/gesture/recognizers.ts
@@ -6,61 +6,85 @@ export interface PanRecognizer {
   getDirection(): number;
 }
 
-export const createPanRecognizer = (direction: string, thresh: number, maxAngle: number): PanRecognizer => {
+export const createPanRecognizer = (direction, thresh, maxAngle) => {
   const radians = maxAngle * (Math.PI / 180);
   const isDirX = direction === 'x';
+  const isDirX = direction === 'y';
+  const isDirXY = direction === 'xy';
+
   const maxCosine = Math.cos(radians);
   const threshold = thresh * thresh;
-
   let startX = 0;
   let startY = 0;
-
   let dirty = false;
-  let isPan = 0;
+  let isPanX = 0;
+  let isPanY = 0;
 
   return {
-    start(x: number, y: number) {
+    start(x, y) {
       startX = x;
       startY = y;
-      isPan = 0;
       dirty = true;
+      isPanX = 0;
+      isPanY = 0;
     },
-
-    detect(x: number, y: number): boolean {
+    detect(x, y) {
+      console.log("asdfasdfasdfass")
       if (!dirty) {
         return false;
       }
-
       const deltaX = (x - startX);
       const deltaY = (y - startY);
       const distance = deltaX * deltaX + deltaY * deltaY;
-
       if (distance < threshold) {
         return false;
       }
       const hypotenuse = Math.sqrt(distance);
-      const cosine = (isDirX ? deltaX : deltaY) / hypotenuse;
 
-      if (cosine > maxCosine) {
-        isPan = 1;
+      if (isDirX || isDirXY) {
+        const cosineX = deltaX / hypotenuse;
+        if (cosineX > maxCosine) {
+          isPanX = 1;
+        }
+        else if (cosineX < -maxCosine) {
+          isPanX = -1;
+        }
+        else {
+          isPanX = 0;
+        }
+      }
 
-      } else if (cosine < -maxCosine) {
-        isPan = -1;
-
-      } else {
-        isPan = 0;
+      if (isDirX || isDirXY) {
+        const cosineY = deltaY / hypotenuse;
+        if (cosineY > maxCosine) {
+          isPanY = 1;
+        }
+        else if (cosineY < -maxCosine) {
+          isPanY = -1;
+        }
+        else {
+          isPanY = 0;
+        }
       }
 
       dirty = false;
       return true;
     },
-
-    isGesture(): boolean {
-      return isPan !== 0;
+    isGesture() {
+      if (isDirX) {
+        return isPanX !== 0;
+      } else if (isDirY) {
+        return isPanY !== 0;
+      }
+      return isPanX !== 0 || isPanY !== 0
     },
-
-    getDirection(): number {
-      return isPan;
+    getDirection() {
+      if (isDirX) {
+        return isPanX;
+      } else if (isDirY) {
+        return isPanY;
+      }
+      return { x: isPanX, y: isPanY }
     }
   };
 };
